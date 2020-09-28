68f6b0fd43aef4f28b06015567c907f4be351880
tharindu <tharindu.perera@ge.com>
2020-09-28 20:38:05 +0530

    new12334

--- src/test/java/MangaServiceUnitTest.java
+++ src/test/java/MangaServiceUnitTest.java
@@ -8,7 +8,8 @@ public class MangaServiceUnitTest {
     @Test
     public void testGetMangasByTitle() throws IOException {
         User user = new User("tharindu");
-        assertEquals("tharindu" ,user.getName());
+//       user.setName("tharindu");
+         assertEquals("tharindu" ,user.getName());
         assertEquals("tharindu" ,user.getName());
 
     }
